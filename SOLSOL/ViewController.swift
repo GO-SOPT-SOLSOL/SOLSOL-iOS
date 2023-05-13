ad7780b146b183c7bc04aa658199a07e40218f95
heejoo <heejoo_byun@naver.com>
2023-05-13 15:32:40 +0900

    Initial Commit

---
+++ SOLSOL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLSOL
+//
+//  Created by 변희주 on 2023/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
