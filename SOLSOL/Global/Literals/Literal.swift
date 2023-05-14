85a621a5e21fda7f87cff6317592576a588f2008
heejoo <heejoo_byun@naver.com>
2023-05-15 00:20:09 +0900

    [Add] #1 - 폴더링 구조 세팅

---
+++ SOLSOL/Global/Literals/Literal.swift
@@ -0,0 +1,8 @@
+//
+//  Literal.swift
+//  SOLSOL
+//
+//  Created by 변희주 on 2023/05/14.
+//
+
+import Foundation
---
+++ SOLSOL/Global/Resources/Font.swift
@@ -0,0 +1,8 @@
+//
+//  Font.swift
+//  SOLSOL
+//
+//  Created by 변희주 on 2023/05/15.
+//
+
+import Foundation
