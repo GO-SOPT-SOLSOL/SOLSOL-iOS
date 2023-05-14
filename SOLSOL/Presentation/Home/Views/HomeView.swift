9f59aecf8143c119e99f8c1f3aee77bf8e1d3c79
heejoo <heejoo_byun@naver.com>
2023-05-15 03:15:59 +0900

    [Add] #1 - 빈폴더에 파일 추가 및 SceneDelegate CodeBase로 변경

---
+++ SOLSOL/Presentation/Home/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  SOLSOL
+//
+//  Created by 변희주 on 2023/05/15.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
