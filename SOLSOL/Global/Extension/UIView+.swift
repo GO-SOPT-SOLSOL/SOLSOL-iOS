09dc7d5cd953023faaa9995c7dcd6001516e364d
heejoo <heejoo_byun@naver.com>
2023-05-15 15:46:53 +0900

    [Add] #3 - Extension안에 있는 UIView+에 makeShadow, makeCornerRound, makeBorder 추가

--- SOLSOL/Global/Extension/UIView+.swift
+++ SOLSOL/Global/Extension/UIView+.swift
@@ -14,3 +14,24 @@ extension UIView {
     }
 }
 
+extension UIView {
+    
+    func makeShadow(radius : CGFloat, offset : CGSize, opacity : Float){
+        layer.shadowColor = UIColor.darkGray.cgColor
+        layer.shadowOffset = offset
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        layer.masksToBounds = false
+    }
+    
+    func makeCornerRound(radius : CGFloat) {
+        layer.cornerRadius = radius
+        layer.masksToBounds = true
+    }
+    
+    func makeBorder(width : CGFloat, color : UIColor) {
+        layer.borderWidth = width
+        layer.borderColor = color.cgColor
+    }
+    
+}
