5fe33e93825db5922582f63744977f8fb2c07275
heejoo <heejoo_byun@naver.com>
2023-05-15 22:50:59 +0900

    [Add] #3 - TransferCollectionViewCell 파일 추

---
+++ SOLSOL/Presentation/Home/Cells/TransferCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TransferCollectionViewCell.swift
+//  SOLSOL
+//
+//  Created by 변희주 on 2023/05/15.
+//
+
+import UIKit
+
+final class TransferCollectionViewCell: UICollectionViewCell {
+    
+}
