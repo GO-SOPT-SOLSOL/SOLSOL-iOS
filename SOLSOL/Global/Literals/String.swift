76351bca7cfcce9e23bf847775d1a49edc49be59
Kim Minjae <ffalswo2@naver.com>
2023-05-16 23:53:31 +0900

    [Add] #4 - StringLiteral 추가

--- SOLSOL/Global/Literals/String.swift
+++ SOLSOL/Global/Literals/String.swift
@@ -12,3 +12,14 @@ enum NavigationBar {
         static let home = "홈"
     }
 }
+
+
+enum TabBar {
+    enum ItemTitle {
+        static let home = "홈"
+        static let moneyVerse = "머니버스"
+        static let product = "상품"
+        static let benefit = "혜택"
+        static let allMenus = "전체 메뉴"
+    }
+}
