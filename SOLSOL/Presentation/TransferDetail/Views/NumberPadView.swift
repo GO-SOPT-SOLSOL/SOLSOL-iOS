c4c21d6d57f552e070c8c7ee2336e46fef4175a7
Kim Minjae <ffalswo2@naver.com>
2023-05-18 00:42:40 +0900

    [Add] #17 - NumberPadView 파일 추가

---
+++ SOLSOL/Presentation/TransferDetail/Views/NumberPadView.swift
@@ -0,0 +1,20 @@
+//
+//  NumberPadView.swift
+//  SOLSOL
+//
+//  Created by 김민재 on 2023/05/17.
+//
+
+import UIKit
+
+class NumberPadView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
