b997aa20288b2f979bc4e773327f6bfd1d0cc6ae
heejoo <heejoo_byun@naver.com>
2023-05-18 01:31:57 +0900

    [Add] #20 - TransferListCollectionViewCell 생성

---
+++ SOLSOL/Presentation/Home/Cells/TransferListCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TransferListCollectionViewCell.swift
+//  SOLSOL
+//
+//  Created by 변희주 on 2023/05/18.
+//
+
+import UIKit
+
+class TransferListCollectionViewCell: UICollectionViewCell {
+    
+}
