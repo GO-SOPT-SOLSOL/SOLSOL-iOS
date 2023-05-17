8815895f3fb8dd208213a1b56ee80afc994b5bb3
heejoo <heejoo_byun@naver.com>
2023-05-18 01:24:58 +0900

    [Feat] #20 - Transfer section 안의 CollectionView 안의 CollectionView 빼고 모두 구현

--- SOLSOL/Presentation/Home/Cells/TransferCollectionViewCell.swift
+++ SOLSOL/Presentation/Home/Cells/TransferCollectionViewCell.swift
@@ -13,7 +13,20 @@ import Then
 final class TransferCollectionViewCell: UICollectionViewCell {
     
     private let transferBackGround = UIView()
-
+    private let shinhanImage = UIImageView()
+    private let depositWithdraw = UILabel()
+    private let solBankBook = UILabel()
+    private let accountNum = UILabel()
+    private let copyButton = UIButton()
+    private let moreButton = UIButton()
+    
+    private let myMoney = UILabel()
+    private let won = UILabel()
+    private let refreshButton = UIButton()
+    private let transferButton = UIButton()
+    private let moneyBoxButton = UIButton()
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -31,16 +44,152 @@ final class TransferCollectionViewCell: UICollectionViewCell {
             $0.backgroundColor = .white
             $0.makeCornerRound(radius: 13)
         }
+        
+        shinhanImage.do {
+            $0.image = ImageLiterals.Home.icBigBankShinhan
+        }
+        
+        depositWithdraw.do {
+            $0.text = "입출금"
+            $0.textColor = .gray600
+            $0.font = .font(.subhead1)
+        }
+        
+        solBankBook.do {
+            $0.text = "쏠편한 입출금통장(저축예금)"
+            $0.textColor = .grayWarm400
+            $0.font = .font(.subhead1)
+        }
+        
+        accountNum.do {
+            $0.text = "신한 110-345-961307"
+            $0.textColor = .grayWarm400
+            $0.font = .font(.body1)
+        }
+        
+        copyButton.do {
+            $0.setImage(ImageLiterals.Home.icCopy, for: .normal)
+        }
+        
+        moreButton.do {
+            $0.setImage(ImageLiterals.Home.icMore, for: .normal)
+        }
+        
+        myMoney.do {
+            $0.text = "3,964,717"
+            $0.textColor = .gray600
+            $0.font = .font(.display5)
+        }
+        
+        won.do {
+            $0.text = "원"
+            $0.textColor = .gray600
+            $0.font = .font(.display4)
+        }
+        
+        refreshButton.do {
+            $0.setImage(ImageLiterals.Home.icRefresh, for: .normal)
+        }
+        
+        transferButton.do {
+            $0.backgroundColor = .blue100
+            $0.setTitle("이체", for: .normal)
+            $0.setTitleColor(.blue500, for: .normal)
+            $0.titleLabel?.font = .font(.subhead2)
+            $0.titleLabel?.textAlignment = .center
+            $0.makeCornerRound(radius: 6)
+        }
+        
+        moneyBoxButton.do {
+            $0.backgroundColor = .blue100
+            $0.setTitle("헤이영 머니박스", for: .normal)
+            $0.setTitleColor(.blue500, for: .normal)
+            $0.titleLabel?.font = .font(.subhead2)
+            $0.titleLabel?.textAlignment = .center
+            $0.makeCornerRound(radius: 6)
+        }
     }
     
     func setLayout() {
         
         contentView.addSubviews(transferBackGround)
+        transferBackGround.addSubviews(shinhanImage,
+                                       depositWithdraw,
+                                       solBankBook,
+                                       accountNum,
+                                       copyButton,
+                                       moreButton,
+                                       myMoney,
+                                       won,
+                                       refreshButton,
+                                       transferButton,
+                                       moneyBoxButton)
         
         transferBackGround.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
         
+        shinhanImage.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().inset(18)
+            $0.trailing.equalToSuperview().inset(285)
+            $0.size.equalTo(36)
+        }
+        
+        depositWithdraw.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(18)
+            $0.leading.equalTo(shinhanImage.snp.trailing).offset(7)
+        }
+        
+        solBankBook.snp.makeConstraints {
+            $0.centerY.equalTo(depositWithdraw.snp.centerY)
+            $0.trailing.equalToSuperview().inset(90)
+        }
+        
+        accountNum.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(38)
+            $0.leading.equalTo(shinhanImage.snp.trailing).offset(7)
+        }
+        
+        copyButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(37)
+            $0.leading.equalTo(accountNum.snp.trailing).offset(2)
+            $0.size.equalTo(14)
+        }
+        
+        moreButton.snp.makeConstraints {
+            $0.top.trailing.equalToSuperview().inset(18)
+            $0.size.equalTo(24)
+        }
+        
+        myMoney.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(86)
+            $0.leading.equalToSuperview().inset(18)
+        }
+        
+        won.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(87.5)
+            $0.leading.equalTo(myMoney.snp.trailing)
+        }
+        
+        refreshButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(150)
+            $0.centerY.equalTo(myMoney.snp.centerY)
+        }
+        
+        transferButton.snp.makeConstraints {
+            $0.top.equalTo(myMoney.snp.bottom).offset(10)
+            $0.leading.equalToSuperview().inset(18)
+            $0.trailing.equalToSuperview().inset(239)
+            $0.height.equalTo(34)
+
+        }
+        
+        moneyBoxButton.snp.makeConstraints {
+            $0.top.equalTo(myMoney.snp.bottom).offset(10)
+            $0.leading.equalToSuperview().inset(107)
+            $0.trailing.equalToSuperview().inset(117)
+            $0.height.equalTo(34)
+        }
     }
 }
 
