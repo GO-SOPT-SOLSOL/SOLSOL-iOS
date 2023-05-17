4162c1a12c176db690fe69c144ad1234eb85eea2
Kim Minjae <ffalswo2@naver.com>
2023-05-18 00:42:59 +0900

    [Add] #17 - 필요한 StringLiterals 추가

--- SOLSOL/Global/Literals/String.swift
+++ SOLSOL/Global/Literals/String.swift
@@ -7,19 +7,39 @@
 
 import Foundation
 
-enum NavigationBar {
-    enum leftItemTitle {
-        static let home = "홈"
+
+enum StringLiterals {
+
+
+    enum NavigationBar {
+        enum leftItemTitle {
+            static let home = "홈"
+        }
+    }
+
+    enum TabBar {
+        enum ItemTitle {
+            static let home = "홈"
+            static let moneyVerse = "머니버스"
+            static let product = "상품"
+            static let benefit = "혜택"
+            static let allMenus = "전체 메뉴"
+        }
+    }
+
+    enum TransferDetail {
+        static let howMuch = "얼마를 보낼까요?"
+        static let amount10000 = "1만"
+        static let amount50000 = "5만"
+        static let amount100000 = "10만"
+        static let amount1000000 = "100만"
+        static let amountTotal = "전액"
+        static let next = "다음"
+        static let transfer = "이체"
+        static let additionalTransfer = "추가이체"
     }
 }
 
 
-enum TabBar {
-    enum ItemTitle {
-        static let home = "홈"
-        static let moneyVerse = "머니버스"
-        static let product = "상품"
-        static let benefit = "혜택"
-        static let allMenus = "전체 메뉴"
-    }
-}
+
+
