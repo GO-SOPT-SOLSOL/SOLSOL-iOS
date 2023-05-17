60f9a5d4e6ddc61420cfd0ed1c8054ec4042c0d0
Kim Minjae <ffalswo2@naver.com>
2023-05-18 00:44:57 +0900

    [Feat] #17 - TransferDetailView 구현

--- SOLSOL/Presentation/TransferDetail/TransferDetailViewController.swift
+++ SOLSOL/Presentation/TransferDetail/TransferDetailViewController.swift
@@ -2,7 +2,7 @@
 //  TransferDetailViewController.swift
 //  SOLSOL
 //
-//  Created by 변희주 on 2023/05/15.
+//  Created by 김민재 on 2023/05/15.
 //
 
 import UIKit
@@ -14,21 +14,20 @@ final class TransferDetailViewController: UIViewController {
 
     private enum Size {
         static let navigationBarheight = 44
+        static let nextButtonHeight = 52
     }
 
     private lazy var navigationBar = SOLNavigationBar(self, leftItem: .back)
 
-    private lazy var button = SOLFilledButton(backgroundColor: .gray150,
-                                         text: "다음",
-                                         textColor: .black,
-                                         image: ImageLiterals.NavigationBar.icArrowLeft,
-                                         imagePlacement: .trailing,
-                                         font: .font(.headline),
-                                              cornerRadius: 14
+    private let transferInfoView = TransferInfoView()
 
-    ).then {
-        $0.addTarget(self, action: #selector(handleTap), for: .touchUpInside)
-    }
+    private let numberPad = NumberPadView()
+
+    private let nextButton = SOLFilledButton(
+        backgroundColor: .blue500,
+        text: StringLiterals.TransferDetail.next,
+        textColor: .white,
+        font: .font(.headline), cornerRadius: 12)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,12 +40,15 @@ final class TransferDetailViewController: UIViewController {
 private extension TransferDetailViewController {
 
     func setStyle() {
-        view.backgroundColor = .gray100
+        self.hidesBottomBarWhenPushed = true
+        view.backgroundColor = .white
     }
 
     func setLayout() {
-        view.addSubview(navigationBar)
-        view.addSubview(button)
+        view.addSubviews(navigationBar,
+                         transferInfoView,
+                         numberPad,
+                         nextButton)
 
         navigationBar.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide)
@@ -54,14 +56,22 @@ private extension TransferDetailViewController {
             make.height.equalTo(Size.navigationBarheight)
         }
 
-        button.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
+        transferInfoView.snp.makeConstraints { make in
+            make.top.equalTo(navigationBar.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+        }
+
+        numberPad.snp.makeConstraints { make in
+            make.top.equalTo(transferInfoView.snp.bottom).offset(15)
             make.leading.trailing.equalToSuperview().inset(18)
         }
-    }
 
-    @objc func handleTap() {
-        print("button tapped !")
+        nextButton.snp.makeConstraints { make in
+            make.top.equalTo(numberPad.snp.bottom)
+            make.leading.trailing.equalToSuperview().inset(18)
+            make.height.equalTo(Size.nextButtonHeight)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 
 }
