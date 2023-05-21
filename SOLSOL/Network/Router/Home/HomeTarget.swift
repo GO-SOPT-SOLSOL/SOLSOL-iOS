401b03722f6421ecec390c5b4ab6258ea4b7e3d8
Kim Minjae <ffalswo2@naver.com>
2023-05-21 12:22:50 +0900

    [Add] #35 - 계좌목록 조회 API Endpoint 정의

--- SOLSOL/Network/Router/Home/HomeTarget.swift
+++ SOLSOL/Network/Router/Home/HomeTarget.swift
@@ -1,8 +1,39 @@
 //
-//  homeTarget.swift
+//  HomeTarget.swift
 //  SOLSOL
 //
 //  Created by 김민재 on 2023/05/21.
 //
 
 import Foundation
+
+import Alamofire
+
+enum HomeTarget {
+    case getAccountsList(_ dto: AccountsListRequestDTO)
+}
+
+extension HomeTarget: TargetType {
+
+    var method: HTTPMethod {
+        switch self {
+        case .getAccountsList(_):
+            return .get
+        }
+    }
+
+    var path: String {
+        switch self {
+        case .getAccountsList(_):
+            return "/accounts/"
+        }
+    }
+
+    var parameters: RequestParams {
+        switch self {
+        case .getAccountsList(let dto):
+            return .requestQuery(dto)
+        }
+    }
+
+}
