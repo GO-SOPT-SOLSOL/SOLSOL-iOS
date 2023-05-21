cc5221ea6b2fceedf3c49b6a8ff2cda11a12f68b
Kim Minjae <ffalswo2@naver.com>
2023-05-21 12:22:29 +0900

    [Add] #35 - 계좌목록조회 API Request, Response 구조체 정의

---
+++ SOLSOL/Network/DTO/Home/Request/AccountsListRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  AccountsListRequestDTO.swift
+//  SOLSOL
+//
+//  Created by 김민재 on 2023/05/21.
+//
+
+import Foundation
+
+struct AccountsListRequestDTO: Encodable {
+    let memberId: Int
+}
