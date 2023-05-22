f0387e79e449e2175b05a117fc733c236a255830
Minjae Kim <ffalswo2@naver.com>
2023-05-23 02:17:56 +0900

    Revert "Revert "[Feat] #24 - 이체확인 UI 구현""

---
+++ SOLSOL/Presentation/TransferConfirm/Views/MemoView.swift
@@ -0,0 +1,108 @@
+//
+//  MemoView.swift
+//  SOLSOL
+//
+//  Created by 김민재 on 2023/05/19.
+//
+
+import UIKit
+
+final class MemoView: UIView {
+
+    @frozen
+    enum MemoViewType {
+        case receive
+        case myBankbook
+        case fee
+    }
+
+    private let type: MemoViewType
+
+    private let memoLabel = UILabel().then {
+        $0.textColor = .gray400
+        $0.font = .font(.body5)
+    }
+
+    private let memoBodyTextField = UITextField().then {
+        $0.placeholder = "미입력시 수취인명"
+        $0.font = .font(.subhead2)
+        $0.textColor = .black
+    }
+
+    private let downArrowButton = SOLFilledButton(
+        backgroundColor: .clear,
+        textColor: .black,
+        image: ImageLiterals.TransferDetail.icArrowDown,
+        font: .font(.subhead2))
+
+    private let feeLabel = UILabel().then {
+        $0.isHidden = true
+        $0.text = "면제"
+        $0.textColor = .black
+        $0.font = .font(.subhead2)
+    }
+
+    init(type: MemoViewType) {
+        self.type = type
+        super.init(frame: .zero)
+        setLayout()
+        setMemoLabel()
+        setStyle()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+
+extension MemoView {
+    private func setStyle() {
+        self.backgroundColor = .white
+    }
+
+    private func setLayout() {
+        addSubviews(memoLabel,
+                    memoBodyTextField,
+                    downArrowButton,
+                    feeLabel)
+
+        memoLabel.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview().inset(17)
+            make.leading.equalToSuperview().inset(11)
+        }
+
+        memoBodyTextField.snp.makeConstraints { make in
+            make.centerY.equalTo(memoLabel)
+            make.leading.equalTo(memoLabel.snp.trailing).offset(17)
+        }
+
+        downArrowButton.snp.makeConstraints { make in
+            make.centerY.equalTo(memoBodyTextField)
+            make.trailing.equalToSuperview().inset(22)
+        }
+
+        feeLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(memoBodyTextField)
+            make.trailing.equalToSuperview().inset(22)
+        }
+
+    }
+
+    func setMemoLabel() {
+        switch type {
+        case .receive:
+            memoLabel.text = "받는분 메모"
+            memoBodyTextField.text = "조정연"
+        case .myBankbook:
+            memoLabel.text = "내통장 메모"
+        case .fee:
+            memoLabel.text = "수수료"
+            memoBodyTextField.isHidden = true
+            feeLabel.isHidden = false
+            downArrowButton.isHidden = true
+        }
+    }
+
+}
--- SOLSOL/Presentation/TransferDetail/Views/TransferDetailView.swift
+++ SOLSOL/Presentation/TransferDetail/Views/TransferDetailView.swift
@@ -16,7 +16,7 @@ final class TransferInfoView: UIView {
         static let myAccountBackgroundViewHeight = 36
     }
 
-    private let receiverLabel = UILabel().then {
+    let receiverLabel = UILabel().then {
         $0.textColor = .gray600
         $0.font = .font(.subhead1)
         // TODO: Transfer에서 데이터 전달 받기
@@ -24,7 +24,7 @@ final class TransferInfoView: UIView {
 
     }
 
-    private let receiverAccountLabel = UILabel().then {
+    let receiverAccountLabel = UILabel().then {
         $0.textColor = .gray400
         $0.font = .font(.body4)
         // TODO: Transfer에서 데이터 전달 받기
@@ -37,7 +37,7 @@ final class TransferInfoView: UIView {
         $0.font = .font(.display6)
     }
 
-    private let moneyLabel = UILabel().then {
+    let moneyLabel = UILabel().then {
         $0.text = StringLiterals.TransferDetail.emptyMoneyLabelText
         $0.textColor = .gray600
         $0.font = .font(.display5)
@@ -123,9 +123,10 @@ extension TransferInfoView {
         }
 
         myAccountBackgroundView.snp.makeConstraints { make in
-            make.top.equalTo(convenientMoneyLabel.snp.bottom).offset(40)
+            make.top.equalTo(convenientMoneyLabel.snp.bottom).offset(40).priority(.low)
             make.leading.trailing.equalToSuperview().inset(18)
             make.height.equalTo(Size.myAccountBackgroundViewHeight)
+            make.bottom.equalToSuperview()
         }
 
         myAccountLabel.snp.makeConstraints { make in
@@ -147,7 +148,7 @@ extension TransferInfoView {
 }
 
 extension TransferInfoView {
-    func configureTransferInfoView(account: MyAccountViewModel) {
+    func configureMyAccount(account: MyAccountViewModel) {
         myAccountLabel.text = "\(account.bank) \(account.myAccount)"
         accountBalance.text = "\(account.balance)원"
     }
@@ -159,4 +160,20 @@ extension TransferInfoView {
     func updateConvenientLabel(text: String) {
         convenientMoneyLabel.text = text
     }
+
+    func setTransferConfirmView() {
+        convenientMoneyLabel.text = "보낼까요?"
+    }
+
+    func setReceiverLabel(name: String, myAccount: String) {
+        receiverLabel.text = "\(name)님께"
+        receiverAccountLabel.text = "\(myAccount)"
+        howMuchLabel.isHidden = true
+
+        moneyLabel.snp.remakeConstraints { make in
+            make.top.equalTo(receiverAccountLabel.snp.bottom).offset(57)
+            make.centerX.equalToSuperview()
+        }
+    }
+
 }
