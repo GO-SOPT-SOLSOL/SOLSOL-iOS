a7fa8e68e8623f18769a52d62945734146e8463b
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2023-05-26 02:36:11 +0900

    [Add] #50 - recentSentAccountListDTO (최근 보낸 사람 목록 조회) 추가

---
+++ SOLSOL/Network/DTO/Transfer/Request/recentSentAccountListRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  recentSentAccountListRequestDTO.swift
+//  SOLSOL
+//
+//  Created by 곽성준 on 2023/05/26.
+//
+
+import Foundation
+
+struct recentSentAccountListRequestDTO: Encodable{
+    let memberId: Int
+}
