aede3a302d96f69ae26378e0b876b1237494ac79
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2023-05-26 03:12:49 +0900

    [Feat] #50 - Router 폴더에 TransferTarget 구현

---
+++ SOLSOL/Network/Router/Transfer/TransferTarget.swift
@@ -0,0 +1,41 @@
+//
+//  TransferTarget.swift
+//  SOLSOL
+//
+//  Created by 곽성준 on 2023/05/26.
+//
+
+import Foundation
+
+import Alamofire
+
+enum TransferTarget{
+    case getRecentSentAccountList(_ dto: RecentSentAccountListRequestDTO)
+}
+
+extension TransferTarget: TargetType{
+    var method: Alamofire.HTTPMethod {
+        switch self{
+        case .getRecentSentAccountList(_):
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self{
+        case .getRecentSentAccountList(_):
+            return "/transfers/"
+        }
+    }
+    
+    var parameters: RequestParams {
+        switch self{
+        case .getRecentSentAccountList(let dto):
+            return .requestQuery(dto)
+        }
+    }
+    
+    
+    
+    
+}
