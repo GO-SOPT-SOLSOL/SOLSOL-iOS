018a3265897f6d5c699c25329d62797107c2cfc5
heejoo <heejoo_byun@naver.com>
2023-05-31 14:09:16 +0900

    [Chore] #58 - Item enum을 string literal로 옮기고 이름을 StackViewItem으로 수정

--- SOLSOL/Global/Literals/StringLiterals.swift
+++ SOLSOL/Global/Literals/StringLiterals.swift
@@ -26,6 +26,17 @@ enum StringLiterals {
             static let allMenus = "전체 메뉴"
         }
     }
+    
+    enum StackViewItem {
+        static let cup = "컵 반환"
+        static let tips = "쏠팁스"
+        static let storyBank = "스토리뱅크"
+        static let wallet = "쏠지갑"
+        static let baseBall = "쏠야구"
+        static let card = "카드 승인"
+        static let stock = "주식현재가"
+        static let certification = "공동인증서"
+    }
 
     enum TransferDetail {
         static let howMuch = "얼마를 보낼까요?"
